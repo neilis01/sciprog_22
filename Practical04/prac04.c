eb247563211481636cd9ab3807fd68d349cc01fc
Neil O'Connor <neiloconnor4@gmail.com>
2022-11-22 13:46:38 +0000

    Addition of extra comments on prac04.c

--- Practical04/prac04.c
+++ Practical04/prac04.c
@@ -1,14 +1,19 @@
+// Import Libraries
 #include <stdio.h>
 #include <math.h>
 
+
+// Function used to convert degrees to radians
 float deg_to_rad(float theta){
 
 	return (M_PI * theta) / 180;
 
 }
 
+// Function used to calculate the Trapezoidal Rule
 float trap_rule(float a, float b, int N, float function_array[N+1]){
 
+	// Local variable declaration
 	float sum = function_array[0] + function_array[N];
 
 	int i = 0;
@@ -24,8 +29,11 @@ float trap_rule(float a, float b, int N, float function_array[N+1]){
 }
 
 
+// Main Function
 int main (void){
 
+
+	// Variable Declaration
 	float start = 0.0;
 	float end = 60;
 	int N = 100000;
@@ -35,6 +43,7 @@ int main (void){
 	
 	int i = 0;
 
+	// Calculating the value of Tan at each point
 	for(i; i <= N; i++){
 
 		tan_arr[i] = tan(deg_to_rad(stepsize*i));		
@@ -43,6 +52,8 @@ int main (void){
 
 
 	float ans = trap_rule(deg_to_rad(start), deg_to_rad(end), N, tan_arr);
+
+	// Print Statements so user can compare answer
 	
 	printf("The answer is %f\n", ans);
 
