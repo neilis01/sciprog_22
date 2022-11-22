f6adf55c4f88679e7f29455ca81afbf64cd31de6
Neil O'Connor <neiloconnor4@gmail.com>
2022-11-22 12:53:35 +0000

    Adding complete codes for Practical 3

---
+++ Practical03/Trap.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+	int N = 12;
+	int i;
+	float a=0.0, b_deg = 60.0;
+	float b_rad, area, mult_rad;
+
+	// Convert b_deg = pi/3 to radians
+
+	b_rad = (M_PI * b_deg)/180.0;
+	printf("pi/3 in Radians = %f\n", b_rad);
+
+	// Sum tan(a) + tan(b)
+
+	area = tan(a) + tan(b_rad);
+	
+	// Calculating area at points x1, x2, ... , x11 by multiplying by 2
+	// Adding them together
+
+	for(i = 5; i < 60; i = i+5){
+		area  = area + 2*tan((M_PI*i)/180.0);	
+	}
+
+	//  Multiply partial area by (pi/3)/(2*12)
+	//  Convert to radians first
+	
+	mult_rad = (M_PI*((b_deg)/(2*N)))/180.0;
+	
+	area = mult_rad*area;
+
+	printf("Approximated = %f, Exact = %f \n", area, log(2.0));
+
+	return 0;
+}
---
+++ Practical03/useprintf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+	int i1 = 10;
+	int i2 = 200;
+
+	float fl1 = 1.11;
+	float fl2 = 2.22;
+
+
+
+	printf("Two ints %d and %d and two floats %f and %f", i1, i2, fl1, fl2);
+
+	return 0;
+
+}
